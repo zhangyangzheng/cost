1d00196229fc49977e9e2b3b6f94928a63bed73c
yangzhengzhang <yangzhengzhang@trip.com>
2022-12-01 19:02:26 +0800

    merger

--- cost-infrastructure/src/main/java/com/ctrip/hotel/cost/infrastructure/mq/AuditMqProducer.java
+++ cost-infrastructure/src/main/java/com/ctrip/hotel/cost/infrastructure/mq/AuditMqProducer.java
@@ -1,6 +1,5 @@
 package com.ctrip.hotel.cost.infrastructure.mq;
 
-import com.ctrip.hotel.cost.domain.data.model.AuditOrderInfoBO;
 import com.ctrip.hotel.cost.infrastructure.model.dto.FgBackToAuditDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
